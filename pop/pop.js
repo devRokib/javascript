31e2e2d2e384ab7a2ea4f90a94dc0e7118dda512
devRokib <rakibahmedra971162@gmail.com>
2023-12-29 17:22:17 +0600

    container confige done

---
+++ pop/pop.js
@@ -0,0 +1,14 @@
+let cities = ["Madrid",'New York',"Khatmandu","Paris"]
+let removeCity = cities.pop()
+console.log(removeCity)
+console.log(cities)
+
+let numbers = [
+    [1,2,3],
+    [4,5,6],
+    [-5,-3,-2],
+
+]
+let proped = numbers.pop()
+console.log(proped)
+console.log(numbers)
