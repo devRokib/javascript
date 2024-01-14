c285204b32725e27de592059d394241427a25e4c
devRokib <rakibahmedra971162@gmail.com>
2024-01-14 16:14:04 +0600

    stackHeap

---
+++ stackAndHeap/stackHeap.js
@@ -0,0 +1,19 @@
+let greeting = 'hello world'
+let anotherGreeting = greeting
+anotherGreeting = "hello bangldesh"
+console.log(anotherGreeting)
+console.log(greeting)
+
+let userOne = {
+    name:'rakib ahmed',
+    email:'rakib@gmail.com',
+    phone: 1746278199
+} 
+let userTwo = userOne
+ userTwo.phone = 1786592137
+ userThree = userTwo
+ userThree.email = 'hello@gmail.com'
+ console.log(userOne.phone)
+ console.log(userTwo.phone)
+ console.log(userOne.email)
+ console.log(userThree.email)
