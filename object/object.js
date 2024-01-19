63b1dfe9378ce7737cc82d17c58628a5cda4aa97
devRokib <rakibahmedra971162@gmail.com>
2024-01-19 17:07:36 +0600

    Object Part One

--- object/object.js
+++ object/object.js
@@ -1,6 +1,41 @@
+const mySym = Symbol()
+const mySym1 = Symbol()
+// console.log(typeof mySym)
+// console.log(mySym === mySym1)
 const myObj =  {
     name:'rakib',
+    'full name':'rakib ahmed',
+    [mySym]:'hello bangladesh',
+     [mySym1]:'hello bangladesh',
+    age:22,
     email:'rakibahmedra971162@gmail.com',
-    inLoggedIn:true,
+    location:'Narsigndi',
+    isLoggedIn:false,
+    lastLoggedIn:['monday','sunday']
 }
-console.log(myObj['email'])
+myObj.email = 'rakib@google.com'
+// Object.freeze(myObj)
+myObj.email = 'rakib@microsoft.com'
+myObj.greeting = function(){
+    console.log('hello user')
+}
+myObj.greetingTwo = function(){
+    return (`hello user ${this['full name']}`)
+}
+myObj.greeting()
+console.log(myObj.greetingTwo())
+// console.log(myObj)
+// console.log(myObj[mySym] === myObj[mySym1])
+// console.log(myObj[mySym])
+// console.log(typeof myObj[mySym])
+// console.log(myObj['email'])
+// console.log(myObj.lastLoggedIn[1])
+// console.log(myObj.isLoggedIn)
+// console.log(myObj["full name"])
+// console.log(myObj["full name"])
+
+// var ab ="var variable  print";
+// function abc(){
+//   return  ('print function')
+// }
+// console.log(abc())
